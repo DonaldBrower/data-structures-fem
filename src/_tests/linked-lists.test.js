11e4a33aad3756fc5aba3757222a050e33b6ea0c
Donald Brower <donald.brower1@gmail.com>
2021-09-04 09:18:00 -0700

    linked lists:   - contains method & tests   - getFirstInstanceOfNode method & tests

--- src/_tests/linked-lists.test.js
+++ src/_tests/linked-lists.test.js
@@ -5,7 +5,7 @@ var { LinkedList } = require("../linked-lists.js");
 test("nodes can be added to the linked list", () => {
   var ll = new LinkedList();
 
-  ll.insertEnd("aaaah");
+  ll.insert("aaaah");
 
   expect(ll._storage[0]).toEqual({
     next: null,
@@ -14,7 +14,7 @@ test("nodes can be added to the linked list", () => {
 
   expect(ll.head).toEqual(0);
 
-  ll.insertEnd("beee");
+  ll.insert("beee");
 
   expect(ll._storage).toEqual({
     0: {
@@ -33,7 +33,7 @@ test("nodes can be added to the linked list", () => {
   expect(ll.head).toEqual(0);
   expect(ll.tail).toEqual(1);
 
-  ll.insertEnd("I'm feeling like a goose");
+  ll.insert("I'm feeling like a goose");
   expect(ll._storage).toEqual({
     0: {
       val: "aaaah",
@@ -62,13 +62,41 @@ test("nodes can be added to the linked list", () => {
   expect(ll.tail).toEqual(2);
 });
 
-test("LinkedList.removeFromEnd removes the very last element and returns it", () => {
+test("remove method - takes in a reference to a node, removes it from the linked list", () => {
   var ll = new LinkedList();
 
-  ll.insertEnd("aaaah");
-  ll.insertEnd("beee");
+  ll.insert("aaaah");
+  ll.insert("beee");
+  ll.insert("ceee3");
 
-  expect(ll.removeFromEnd()).toEqual({ val: "beee", next: null });
+  var node = ll.contains("beee");
+
+  expect(ll.remove(node)).toEqual({ val: "beee", next: null });
+  expect(ll._storage).toEqual({
+    0: {
+      val: "aaaah",
+      next: {
+        val: "ceee3",
+        next: null,
+      },
+    },
+    2: {
+      val: "ceee3",
+      next: null,
+    },
+  });
+
+  expect(ll.head).toEqual(0);
+  expect(ll.tail).toEqual(0);
+});
+
+test("remove method takes in a value, searches the linked list for the value, and if it's there, remove and return it", () => {
+  var ll = new LinkedList();
+
+  ll.insert("aaaah");
+  ll.insert("beee");
+
+  expect(ll.remove()).toEqual({ val: "beee", next: null });
   expect(ll._storage).toEqual({
     0: {
       val: "aaaah",
@@ -79,3 +107,73 @@ test("LinkedList.removeFromEnd removes the very last element and returns it", ()
   expect(ll.head).toEqual(0);
   expect(ll.tail).toEqual(0);
 });
+
+test("removeTail method removes the last node in the linked list", () => {
+  var ll = new LinkedList();
+
+  ll.insert("aaaah");
+  ll.insert("beee");
+
+  expect(ll.removeTail()).toEqual({ val: "beee", next: null });
+  expect(ll._storage).toEqual({
+    0: {
+      val: "aaaah",
+      next: null,
+    },
+  });
+
+  expect(ll.head).toEqual(0);
+  expect(ll.tail).toEqual(0);
+});
+
+test("contains(value) returns true if there is a node in the list with a value property equal to the argument value", () => {
+  var ll = new LinkedList();
+  ll.insert("aaaah");
+  ll.insert("baaah");
+  ll.insert("caaah");
+  ll.insert("daaah");
+
+  /* expect(ll.contains("baaah")).toEqual({
+    1: {
+      val: "baaah",
+      next: { val: "caaah", next: { val: "daaah", next: null } },
+    },
+    2: {
+      val: "caaah",
+      next: {
+        val: "daaah",
+        next: null,
+      },
+    },
+    3: {
+      val: "daaah",
+      next: null,
+    },
+  }); */
+
+  expect(ll.contains("baaah")).toEqual(true);
+  expect(ll.contains("caaah")).toEqual(true);
+  expect(ll.contains("aaaah")).toEqual(true);
+  expect(ll.contains("daaah")).toEqual(true);
+});
+
+test("getFirstInstanceOfNode(value) returns the node's val if there is a node in the list with a value property equal to the argument value, false if there isn't", () => {
+  var ll = new LinkedList();
+  ll.insert("aaaah");
+  ll.insert("baaah");
+  ll.insert("caaah");
+  ll.insert("daaah");
+
+  expect(ll.getFirstInstanceOfNode("baaah")).toEqual({
+    val: "baaah",
+    next: { val: "caaah", next: { val: "daaah", next: null } },
+  });
+
+  expect(ll.getFirstInstanceOfNode("caaah")).toEqual({
+    val: "caaah",
+    next: { val: "daaah", next: null },
+  });
+});
+test("isHead takes in a reference to a node and checks if it's the head of the linked list`", () => {});
+
+test("isTail takes in a reference to a node and checks if it's the tail of the linked list`", () => {});
--- src/linked-lists.js
+++ src/linked-lists.js
@@ -7,26 +7,28 @@ function LinkedList() {
   this.head = null;
   this.tail = null;
 }
-LinkedList.prototype.insertEnd = insertEnd;
-LinkedList.prototype.removeFromEnd = removeFromEnd;
+LinkedList.prototype.insert = insert;
+LinkedList.prototype.remove = remove;
+LinkedList.prototype.contains = contains;
+LinkedList.prototype.getFirstInstanceOfNode = getFirstInstanceOfNode;
 
 function LinkedListNode(v, next = null) {
   this.val = v;
   this.next = next;
 }
 
-//*********************************
-function insertEnd(v) {
+//**********************************
+function insert(v) {
   /**
    *
    */
   if (this.head === null) {
     // when the head is null it's assumed the storage is empty. start from zero.
-    this._storage[0] = new LinkedListNode(v);
+    this._storage[0] = { val: v, next: null };
     (this.head = 0), (this.tail = 0);
   } else {
     // create a new node in storage
-    this._storage[this.tail + 1] = new LinkedListNode(v);
+    this._storage[this.tail + 1] = { val: v, next: null };
 
     // assign the new node to the old tail's `next` property
     this._storage[this.tail].next = this._storage[this.tail + 1];
@@ -36,13 +38,12 @@ function insertEnd(v) {
   }
 }
 
-function removeFromEnd() {
+function remove() {
   if (this.head === null) {
     return null;
   } else {
     return recurse(this.storage[this.head]);
   }
-
   //**************
   var prevNode;
   function recurse(node) {
@@ -57,5 +58,27 @@ function removeFromEnd() {
         recurse(node.next);
       }
     }
+  }
+}
+
+function contains(v, r) {
+  return recurse(this._storage[this.head]);
+  function recurse(node) {
+    if (node.val === v) {
+      return true;
+    } else if (node.next !== null) {
+      return recurse(node.next);
+    } else return false;
+  }
+}
+
+function getFirstInstanceOfNode(v) {
+  return recurse(this._storage[this.head]);
+  function recurse(node) {
+    if (node.val === v) {
+      return node;
+    } else if (node.next !== null) {
+      return recurse(node.next);
+    } else return false;
   }
 }
