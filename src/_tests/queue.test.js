466c100bc803af6f8a416e568fb440e82f87d65d
Donald Brower <donald.brower1@gmail.com>
2021-09-03 18:17:10 -0700

    dequeue, tests

--- src/_tests/queue.test.js
+++ src/_tests/queue.test.js
@@ -14,3 +14,23 @@ test("an item can be queued up", () => {
   expect(queue.front).toEqual(0);
   expect(queue.back).toEqual(1);
 });
+
+test("", () => {
+  var queue = new Queue();
+  queue.enqueue("goat");
+  queue.enqueue("llama");
+  expect(queue.front).toEqual(0);
+  expect(queue.back).toEqual(1);
+
+  expect(queue.dequeue()).toEqual("goat");
+  expect(queue._storage[0] === undefined);
+  expect(queue.front).toEqual(1);
+  expect(queue.back).toEqual(1);
+
+  expect(queue.dequeue()).toEqual("llama");
+  expect(queue.front).toEqual(null);
+  expect(queue.back).toEqual(null);
+
+  expect(queue.dequeue()).toEqual(null);
+  expect(queue.dequeue()).toEqual(null);
+});
--- src/queue.js
+++ src/queue.js
@@ -18,4 +18,23 @@ Queue.prototype.enqueue = function enqueue(v) {
   }
 };
 
-Queue.prototype.dequeue = function dequeue() {};
+Queue.prototype.dequeue = function dequeue() {
+  if (this.front === null && this.back === null) {
+    return null;
+  }
+
+  var dequeuedEl;
+  if (this.front - this.back < 0) {
+    dequeuedEl = this._storage[this.front];
+    delete this._storage[this.front];
+
+    this.front++;
+  } else {
+    dequeuedEl = this._storage[this.front];
+    delete this._storage[this.front];
+
+    (this.front = null), (this.back = null);
+  }
+
+  return dequeuedEl;
+};
